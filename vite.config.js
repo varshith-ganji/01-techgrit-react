10e4d79f26e8f3819086125263ffcf42892259a4
varshith-ganji <varshithreddyganji852@gmail.com>
2025-02-25 10:03:17 -0600

    modified vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ensure relative paths for assets
+  base: "/01-techgrit-react/", // Ensure relative paths for assets
   build: {
     outDir: "docs", // Change the build output directory to 'docs'
   },
